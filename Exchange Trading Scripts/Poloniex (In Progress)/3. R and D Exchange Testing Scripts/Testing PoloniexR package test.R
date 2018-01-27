e853284f3cdc366f77a13aa771a6da01efa1cec6
x78163 <joseph.difrancesco@gmail.com>
2018-01-27 15:43:04 +0100

    testing...

--- Exchange Trading Scripts/Poloniex (In Progress)/3. R and D Exchange Testing Scripts/Testing PoloniexR package test.R
+++ Exchange Trading Scripts/Poloniex (In Progress)/3. R and D Exchange Testing Scripts/Testing PoloniexR package test.R
@@ -4,9 +4,10 @@ poloniex.public <- PoloniexPublicAPI()
 ticker.info     <- ReturnTicker(poloniex.public)
 head(ticker.info)
 
+pol = read.csv("auth.csv")
 
-key    = ""
-secret = ""
+key    = as.character(pol$key)
+secret = as.character(pol$secret)
 poloniex.trading <- PoloniexTradingAPI(key    = key,
                                        secret = secret)
 balances <- ReturnBalances(poloniex.trading)
