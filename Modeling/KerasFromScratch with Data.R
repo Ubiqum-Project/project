e1542255f3541f928ab0923d0726efa791ed1ad8
x78163 <joseph.difrancesco@gmail.com>
2018-02-22 09:32:33 +0100

    uploading functional model

--- Modeling/KerasFromScratch with Data.R
+++ Modeling/KerasFromScratch with Data.R
@@ -2,10 +2,13 @@ library(keras)
 library(OneR)
 library(Hmisc)
 library(caret)
-bins = 2
+library(binr)
+bins = 3
+splitLower = 1100
+splitHigher = splitLower +1
 
 
-data = read.csv(gzfile("finalRate.csv.gz"))
+#data = read.csv(gzfile("finalRate.csv.gz"))
 data = read.csv(gzfile("finalValue.csv.gz"))
 data = data+1
 data = data[ , apply(data, 2, function(x) !any(is.na(x)))]
@@ -15,25 +18,32 @@ data=data[,3:ncol(data)]
 data$AveragedExchange = as.numeric(as.character(data$AveragedExchange))
 
 test = data
-test$bin = as.numeric(cut2(data$AveragedExchange, g=bins))
+test$bin = as.numeric(cut2(data$AveragedExchange, g=bins, m=nrow(data)/bins))
+
+# library(classInt)
+# classIntervals(data$AveragedExchange, 4, style = 'quantile')
+# x <- as.data.frame(classIntervals(data$AveragedExchange, 4, style = 'equal'))
 
 data$AveragedExchange = as.numeric(cut2(data$AveragedExchange, g=bins))
-
+plot(bin(data$AveragedExchange, bins))
+summary(data$AveragedExchange)
 data$AveragedExchange= as.numeric(data$AveragedExchange)-1
 
 data$AveragedExchange = as.factor(data$AveragedExchange)
+plot(data$AveragedExchange)
+x_train = (data[1:splitLower,1:ncol(data)-1])
+y_train = (data[1:splitLower,ncol(data)])
+x_test = data[splitHigher:nrow(data),1:ncol(data)-1]
+y_test = data[splitHigher:nrow(data),ncol(data)]
 
-x_train = (data[1001:nrow(data),1:ncol(data)-1])
-y_train = (data[1001:nrow(data),ncol(data)])
-
-x_test = data[1:1000,1:ncol(data)-1]
-y_test = data[1:1000,ncol(data)]
 
 x_train <- as.data.frame(x_train)
 y_train <- as.data.frame(y_train)
 x_test <- as.data.frame(x_test)
 y_test <- as.data.frame(y_test)
 
+plot(y_test)
+plot(y_train)
 trainingSize = ncol(x_train)
 
 
@@ -77,7 +87,7 @@ model %>% evaluate(x_test, y_test)
 
 model %>% predict_classes(x_train)
 predictions = round(as.data.frame(predict(model,x_test)), digits = 1)
-colnames(predictions) =c("0","1")
+colnames(predictions) =c("0","1","2")
 predictions$predicted = colnames(predictions)[apply(predictions,1,which.max)]
 predictions$actual = y_test
 predictions$compare = (predictions$predicted == predictions$actual)
