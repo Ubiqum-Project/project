e51826ebb5f0e1f1b94a0da0fe0d9c8d46e7cbaa
x78163 <joseph.difrancesco@gmail.com>
2018-03-02 13:26:19 +0100

    updated the simulator

---
+++ cointrader/app.R
@@ -0,0 +1,302 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+#install.packages('shiny')
+#install.packages('coindeskr')
+#install.packages('dygraphs')
+
+library(shiny) #To build the shiny App
+library(coindeskr) #R-Package connecting to Coindesk API 
+library(dygraphs) #For interactive Time-series graphs
+library(shinydashboard)
+library(ggplot2)
+#----------------------------------------------------------
+
+
+gg.gauge <- function(pos,breaks=c(0,30,50,70,100)) {
+  require(ggplot2)
+  get.poly <- function(a,b,r1=0.5,r2=1.0) {
+    th.start <- pi*(1-a/100)
+    th.end   <- pi*(1-b/100)
+    th       <- seq(th.start,th.end,length=100)
+    x        <- c(r1*cos(th),rev(r2*cos(th)))
+    y        <- c(r1*sin(th),rev(r2*sin(th)))
+    return(data.frame(x,y))
+  }
+  ggplot()+ 
+    geom_polygon(data=get.poly(breaks[1],breaks[2]),aes(x,y),fill="red")+
+    geom_polygon(data=get.poly(breaks[2],breaks[3]),aes(x,y),fill="orange")+
+    geom_polygon(data=get.poly(breaks[3],breaks[4]),aes(x,y),fill="lightgreen")+
+    geom_polygon(data=get.poly(breaks[4],breaks[5]),aes(x,y),fill="forestgreen")+
+    geom_polygon(data=get.poly(pos-1,pos+1,0.2),aes(x,y))+
+    geom_text(data=as.data.frame(breaks), size=5, fontface="bold", vjust=0,
+              aes(x=1.1*cos(pi*(1-breaks/100)),y=1.1*sin(pi*(1-breaks/100)),label=paste0(c("SELL!!", "sell","No Action", "buy", "BUY!!"),"")))+
+    annotate("text",x=0,y=0,label=pos,vjust=0,size=8,fontface="bold")+
+    coord_fixed()+
+    theme_bw()+
+    theme(axis.text=element_blank(),
+          axis.title=element_blank(),
+          axis.ticks=element_blank(),
+          panel.grid=element_blank(),
+          panel.border=element_blank()) 
+}
+
+
+#-----------------------------------------------------------
+
+transactions = read.csv("transactions.csv")[,-1]
+last31 <- get_last31days_price()
+transactions$date = as.character(transactions$date)
+transactions$walletValue= as.numeric(transactions$walletValue)
+
+
+current = public_ticker(product_id = "BTC-USD")
+
+
+#-----------------------------------------------------------
+
+
+# Define UI for application that draws a histogram
+ui <- dashboardPage(
+  dashboardHeader(),
+  dashboardSidebar(),
+  dashboardBody(
+    fluidRow(
+    titlePanel('Bitcoin USD Price for Last 31 days'),
+    tableOutput("values"),
+    mainPanel(
+      h3('Minimum'),
+      h3(htmlOutput('minprice')),
+      h3('Maximum'),
+      h3(htmlOutput('maxprice')),
+     box( dygraphOutput("btcprice")),
+     box(
+       title = "Controls",
+       sliderInput("sentiment",  label = div(style='width:250px;', 
+                                           "This is the Output of the Sentiment Model",
+                                           br(),
+                                           div(style='float:left;', 'Decrease'), 
+                                           div(style='float:right;', 'Increase')),  -1, 1, 0, step = .5),
+       plotOutput('sentGauge'),
+        sliderInput("technical",  label = div(style='width:250px;', 
+                                           "This is the Output of the Technical Model",
+                                           br(),
+                                           div(style='float:left;', 'Decrease'), 
+                                           div(style='float:right;', 'Increase')),  -1, 1, 0, step = .5),
+       plotOutput('techGauge'),
+       sliderInput("riskBTC",  label = div(style='width:250px;', 
+                                           "What % of BTC Wallet Available to Trade?",
+                                           br(),
+                                           div(style='float:left;', '0%'), 
+                                           div(style='float:right;', '100%')),  0, 100, 10),
+       sliderInput("riskUSD", label = div(style='width:250px;', 
+                                          "What % of USD Wallet Available to Trade?",
+                                          br(),
+                                          div(style='float:left;', '0%'), 
+                                          div(style='float:right;', '100%')), 1, 100, 10),
+       sliderInput("sentVtech", label = div(style='width:250px;', 
+                                            "What Analysis Algorithm Would You Like to Favor (5.5 means equal weight)",
+                                            br(),
+                                            div(style='float:left;', 'Sentiment'), 
+                                            div(style='float:right;', 'Technical')), 1, 10, 5.5, step = .5),
+       plotOutput('combiGauge'),
+       textOutput('recommendation')
+       
+     )
+    )
+  )))
+
+# Define server logic required to draw a histogram
+server <- function(input,output){
+  
+  sliderValues <- reactive({
+ 
+    bid = current$bid
+    ask = current$ask
+    price = current$price
+    vol = current$volume
+    
+    date =as.character(current$time)
+    
+    
+    
+    btcAlert = "No BTC Wallet Constraints!"
+    usdAlert = "No USD Wallet Constraints!"
+    technical= input$technical  # Range of 1,.5, 0, -.5, -1  These Feed in from Algorithms
+    sentiment = input$sentiment # Range of 1,.5, 0, -.5, -1   These Feed in from Algorithms
+    print(paste("technical:",technical))
+    print(paste("sentiment:",sentiment))
+    riskBTC = input$riskBTC/100        #User defined value (basically percentage of wallet available for transaction) 
+    riskUSD = input$riskUSD/100        #User defined value (basically percentage of wallet available for transaction)
+    print(paste("riskUSD:",riskUSD))
+    print(paste("riskBTC:",riskBTC))
+    sentVtech = input$sentVtech       #User defined value (the amount of weight each algo will receive: 1 = full sentiment, 9 = full technical, 5 = equal mix)
+    print(paste("sentVtech:",sentVtech))
+    #---------> Combines the user defined weights to identify a multiplier <------------------
+    sentVal = sentiment*((10- sentVtech)/10)
+    techVal = technical*(sentVtech/10)
+    combiVal = techVal+sentVal
+    print(paste("sentVal:",sentVal))
+    print(paste("techVal:",techVal))
+    print(paste("combiVal:",combiVal))
+    
+    #---------> Calculates amount of wallet available to use <------------------
+    walletUSD =as.numeric(transactions$walletUSD[nrow(transactions)])
+    print(paste("walletUSD:",walletUSD))
+    walletBTC =as.numeric(transactions$walletBTC[nrow(transactions)])
+    print(paste("walletBTC:",walletBTC))
+    
+    playUSD=walletUSD *riskUSD
+    playBTC=walletBTC*riskBTC
+    print(paste("playUSD:",playUSD))
+    print(paste("playBTC:",playBTC))
+    
+    playUSDAvailable = playUSD
+    playBTCAvailable = playBTC
+    
+    
+    
+    if((playUSD/price) < .001)
+    {
+      if(walletUSD > .001*price)
+      {
+        usdAlert = paste("Oops, you selected a USD wallet risk percentage that GDAX does not support:",round(playUSD/price, digits =4),"BTC.  I am going to default you to .001 BTC, the minimum for a trade")
+        playUSDAvailable = .001*price
+      } else { playUSDAvailable = 0
+      usdAlert = paste("Oops, your wallet has insufficient funds to meet the .001 BTC trade minimum.  ")
+      }
+    }
+    
+    if((playBTC) < .001)
+    {
+      if(walletBTC > .001)
+      {
+        playBTCAvailable = .001
+      } else { playBTCAvailable = 0
+      usdAlert = paste("Oops, your wallet has insufficient funds to meet the .001 BTC trade minimum.  ")
+      }
+    }
+    
+    print(paste("playUSDAvailable:",playUSDAvailable))
+    print(paste("playBTCAvailable:",playBTCAvailable))
+    btcXaction = 0
+    usdXaction = 0
+    
+    canBuy = playUSDAvailable/price 
+    canSell = playBTCAvailable*price
+    
+    print(paste("canBuy:",canBuy))
+    print(paste("canSell:",canSell))
+    recommendation =0
+    #----------> Calculates the appropriate move <---------------
+    
+    if (combiVal == 0)
+    {
+      recommendation =paste("Based upon your selections and the sentiment/technical indicators from the last week, it appears there will be very little change in Bitcoin price over the next 24 hours to justify any position change.  Therefore, I recommend no transactions. If you wish to log a transaction of zero, go ahead anc click the submit order button below. ")
+      walletBTC = walletBTC
+      walletUSD = walletUSD
+    }else if(combiVal > 0 && canBuy>0)
+    {
+      
+      walletBTC = walletBTC+canBuy
+      walletUSD = walletUSD-playUSD
+      recommendation =paste("I think you should buy some Bitcoin.  Based upon sentiment and technical indicators from the past week, it appears that Bitcoin price will increase over the next 24 hour period.  I recommend you buy",round(canBuy, digits = 4), "bitcoin for $", round(playUSD, digits = 2), ".  That would put your bitcoin wallet at:", round(walletBTC, digits = 4), "BTC and your USD wallet at: $", round(walletUSD, digits = 2),".  If you agree with this transaction (Buy Low), click the submit order button below.")
+      btcXaction = +canBuy
+      usdXaction = -playUSD
+    }else if(combiVal < 0 && canSell>0)
+    {
+      
+      walletBTC = walletBTC-playBTC
+      walletUSD =walletUSD +canSell
+      btcXaction = -playBTC
+      usdXaction = +canSell
+      recommendation = paste("I think you should sell some Bitcoin.  Based upon sentiment and technical indicators from the past week, it appears that Bitcoin price will decrease over the next 24 hour period.  I recommend you sell",round(playBTC, digits = 4), "bitcoin for $", round(canSell, digits = 2), ".  That would put your bitcoin wallet at:", round(walletBTC, digits = 4), "BTC and your USD wallet at: $", round(walletUSD, digits = 2),".  If you agree with this transaction (Sell High), click the submit order button below.")
+    } else { recommendation = paste("Trade denied to to insuficient funds:", round(canBuy, digits = 4),"USD and ", round(canSell, digits = 4),"BTC")}
+    
+    print(paste("walletBTC:",walletBTC))
+    print(paste("walletUSD:",walletUSD))
+    print(paste("btcXaction:",btcXaction))
+    print(paste("usdXaction:",usdXaction))
+    walletValue = (price*walletBTC)+walletUSD
+    print(paste("walletValue:",walletValue))
+    
+    delta = walletValue-as.numeric(transactions$walletValue[nrow(transactions)-1]) #change in wallet value from previous
+   value = data.frame(date,  bid, ask, price, vol, technical, sentiment, sentVtech, riskBTC, riskUSD, btcXaction, usdXaction,walletBTC,walletUSD, walletValue, delta, recommendation)
+    # transactions[nrow(transactions) + 1,] = c(date,  bid, ask, price, vol, technical, sentiment, sentVtech, riskBTC, riskUSD, btcXaction, usdXaction,walletBTC,walletUSD, walletValue, delta)
+    # 
+    # write.csv(transactions, file = "transactions.csv")
+    #print(recommendation)
+    print(paste("recommendation: ",value$recommendation))
+    return(value)
+    
+   
+    
+  })
+  output$values <- renderTable({
+    sliderValues()
+  })
+  
+  output$recommendation <- renderText({
+    data=sliderValues()
+    response = as.character(data[1,ncol(data)])
+    return(response)
+  })
+  output$sentGauge = renderPlot({
+    data = sliderValues()
+    sentVal = data$sentiment
+    sentNorm = ((sentVal+1)/2)*100
+   sentGauge= gg.gauge(sentNorm,breaks=c(0,30,50,70,100))
+   sentGauge
+  })
+  
+  output$techGauge = renderPlot({
+    data = sliderValues()
+    sentVal = data$technical
+    sentNorm = ((sentVal+1)/2)*100
+    techGauge= gg.gauge(sentNorm,breaks=c(0,30,50,70,100))
+    techGauge
+  })
+  
+  output$combiGauge = renderPlot({
+    data = sliderValues()
+    technical = data$technical
+    sentiment = data$sentiment
+    sentVtech = input$sentVtech       #User defined value (the amount of weight each algo will receive: 1 = full sentiment, 9 = full technical, 5 = equal mix)
+      sentVal = sentiment*((10- sentVtech)/10)
+    techVal = technical*(sentVtech/10)
+    combiVal = techVal+sentVal
+    sentNorm = ((combiVal+1)/2)*100
+    combiGauge= gg.gauge(sentNorm,breaks=c(0,30,50,70,100))
+    combiGauge
+  })
+  #------------->>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+  output$minprice <- renderText(
+    paste('Price : $', min(last31), '
+          Date :', rownames(last31)[which.min(last31$Price)] )
+    )
+  
+  
+  output$maxprice <- renderText(
+    paste('Price : $', max(last31), '
+          Date :', rownames(last31)[which.max(last31$Price)] )
+    )
+  output$btcprice <- renderDygraph(
+    dygraph(data = last31, main = "Bitcoin USD Price for Last 31 days") %>% 
+      dyHighlight(highlightCircleSize = 5, 
+                  highlightSeriesBackgroundAlpha = 0.2,
+                  hideOnMouseOut = FALSE, highlightSeriesOpts = list(strokeWidth = 3)) %>%
+      dyRangeSelector()
+  )
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+
+
+
