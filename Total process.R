bcb92e315d3a2e4ff024610f04d8bb365cd87526
Arjen Van den Heuvel <arjenvandenheuvel@gmail.com>
2018-03-06 11:05:28 +0100

    new total process with better time logging

--- Total process.R
+++ Total process.R
@@ -7,37 +7,37 @@ TX<-Sys.time()
 #Cleaning and streamlining
 print(paste("Cleaning and streamlining"))
 source("Cleaning/Date Time Cleaning/StreamliningDateTime.R")
-print(paste("TOTAL TIME PROGRAM : ",difftime(TT, Sys.time(),units = "auto")))
-print(paste("Cleaning and streamlining : ",difftime(TX, Sys.time())))
+print(paste("TOTAL TIME PROGRAM : ", round(difftime(TT, Sys.time(),units = "mins"),2),"minutes"))
+print(paste("Cleaning and streamlining : ",round(difftime(TX, Sys.time(),units = "mins"),2),"minutes"))
 TX<-Sys.time()
 
 #Sentiment
 print(paste("Sentiment"))
 source("Sentiment Engines/Main Running.R")
-print(paste("TOTAL TIME PROGRAM : ",difftime(TT, Sys.time(),units = "auto")))
-print(paste("Sentiment : ",difftime(TX, Sys.time())))
+print(paste("TOTAL TIME PROGRAM : ", round(difftime(TT, Sys.time(),units = "mins"),2),"minutes"))
+print(paste("Sentiment : ",round(difftime(TX, Sys.time(),units = "mins"),2),"minutes"))
 TX<-Sys.time()
 
 #Secondary predictors
 print(paste("Secondary predictors"))
 source("Other Predictors/Stock Puller.R")
-print(paste("TOTAL TIME PROGRAM : ",difftime(TT, Sys.time(),units = "auto")))
-print(paste("Secondary predictors : ",difftime(TX, Sys.time())))
+print(paste("TOTAL TIME PROGRAM : ", round(difftime(TT, Sys.time(),units = "mins"),2),"minutes"))
+print(paste("Secondary predictors : ",round(difftime(TX, Sys.time(),units = "mins"),2),"minutes"))
 TX<-Sys.time()
 
 #Data Normalization
 print(paste("Data Normalization"))
 source("Modeling/dataNormalizer.R")
-print(paste("TOTAL TIME PROGRAM : ",difftime(TT, Sys.time(),units = "auto")))
-print(paste("Data Normalization : ",difftime(TX, Sys.time())))
+print(paste("TOTAL TIME PROGRAM : ", round(difftime(TT, Sys.time(),units = "mins"),2),"minutes"))
+print(paste("Data Normalization : ",round(difftime(TX, Sys.time(),units = "mins"),2),"minutes"))
 TX<-Sys.time()
 
 #Models
 print(paste("Models"))
 source("XXXXXXXXXXXXX.R")
-print(paste("TOTAL TIME PROGRAM : ",difftime(TT, Sys.time(),units = "auto")))
-print(paste("Models : ",difftime(TX, Sys.time())))
+print(paste("TOTAL TIME PROGRAM : ", round(difftime(TT, Sys.time(),units = "mins"),2),"minutes"))
+print(paste("Models : ",round(difftime(TX, Sys.time(),units = "mins"),2),"minutes"))
 TX<-Sys.time()
 
 #TOTAL TIME
-print(paste("TOTAL TIME PROGRAM : ",difftime(TT, Sys.time(),units = "auto")))
+print(paste("TOTAL TIME PROGRAM : ", round(difftime(TT, Sys.time(),units = "mins"),2),"minutes"))
